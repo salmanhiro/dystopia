391de1834b63eb667cdde023bb8b725095180331
salmanhiro <salmanhiro10@gmail.com>
2022-12-17 20:09:55 +0700

    Add lite

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ FROM python:3.9
 # Install Streamlit and other dependencies
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-
-RUN pip install protobuf~=4.21.1
 # Add the app files to the image
 ADD . /src
 
 # Set the working directory to the app directory
 WORKDIR /src
+
+EXPOSE 8501
 
 # Run the app
 CMD streamlit run src/app.py
--- src/app.py
+++ src/app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import os
 import openai
 import string
-import tensorflow_hub as hub
 import numpy as np
-import tensorflow as tf
-from PIL import Image
 import random
 
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
@@ -46,20 +43,6 @@ def generate_story(year):
     story = completion["choices"][0]["text"].strip('\n')
     return story
 
-def generate_nightmare(content_image, style_image):
-    content_image = np.asarray(content_image).astype(np.float32)[np.newaxis, ...] / 255.
-    style_image = np.asarray(style_image).astype(np.float32)[np.newaxis, ...] / 255.
-    # Optionally resize the images. It is recommended that the style image is about
-    # 256 pixels (this size was used when training the style transfer network).
-    # The content image can be any size.
-    style_image = tf.image.resize(style_image, (256, 256))
-
-    # Load image stylization module.
-    hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-    stylized_image = outputs[0]
-    return stylized_image
-
 st.header("Future dystopian story")
 
 
@@ -71,19 +54,6 @@ words = generate_story(year)
 
 # Use Streamlit to display the random word
 st.write("OpenAI prompted:", words)
-
-st.header("Nightmare visualization")
-
-file = st.file_uploader("Choose an image file")
-
-if file:
-    content_image = Image.open(file)
-    random_style = random.choice(os.listdir("src/styles")) #change dir name to whatever
-    style_image = Image.open(f'src/styles/{random_style}')
-    output_tensor = generate_nightmare(content_image, style_image)[0].numpy()
-    st.image(output_tensor, caption="Generated dystopian image", use_column_width=True)
-
-
 
 # Add a footer
 st.markdown(
